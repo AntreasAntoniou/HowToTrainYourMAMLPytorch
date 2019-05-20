95007dc4b9240027bfc5f19a46ffec9a74e194e4
AntreasAntoniou <antrikohs@gmail.com>
2019-05-20 03:12:47 +0100

    Add cleaner and more robust inner loop optimizers

--- experiment_scripts/mini-imagenet_maml++-mini-imagenet_5_2_0.01_48_5_1_few_shot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ..
-export DATASET_DIR="../datasets/"
-# Activate the relevant virtual environment:
-
-python train_maml_system.py --name_of_args_json_file experiment_config/mini-imagenet_maml++-mini-imagenet_5_2_0.01_48_5_1.json --gpu_to_use 0
--- experiment_scripts/mini-imagenet_maml-mini-imagenet_1_2_0.01_32_5_0_few_shot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ..
-export DATASET_DIR="../datasets/"
-# Activate the relevant virtual environment:
-
-python train_maml_system.py --name_of_args_json_file experiment_config/mini-imagenet_maml-mini-imagenet_1_2_0.01_32_5_0.json --gpu_to_use 0
