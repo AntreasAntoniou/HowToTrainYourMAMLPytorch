b4fc81f357ab40f0145954caf094cfb41d549c2e
AntreasAntoniou <antrikohs@gmail.com>
2019-05-21 16:30:19 +0100

    Fix dataset paths

--- script_generation_tools/local_run_template_script.sh
+++ script_generation_tools/local_run_template_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
-export DATASET_DIR="../datasets/"
+export DATASET_DIR="datasets/"
 # Activate the relevant virtual environment:
 
 python $execution_script$ --name_of_args_json_file experiment_config/$experiment_config$ --gpu_to_use 0
