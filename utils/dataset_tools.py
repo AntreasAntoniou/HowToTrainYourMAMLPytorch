6c0490ed63be987d74c0a7884a215e0e6f8cc48e
Antreas Antoniou <antrikohs@gmail.com>
2019-07-24 12:27:43 +0100

    Update dataset_tools.py

--- utils/dataset_tools.py
+++ utils/dataset_tools.py
@@ -38,7 +38,7 @@ def maybe_unzip_dataset(args):
                 total_files == 3 and 'mini_imagenet_pkl' in datasets[dataset_idx]):
             print("file count is correct")
             done = True
-        elif datasets[dataset_idx] != 'omniglot_dataset' and datasets[dataset_idx] != 'mini_imagenet' and datasets[dataset_idx] and datasets[dataset_idx] != 'mini_imagenet_pkl':
+        elif datasets[dataset_idx] != 'omniglot_dataset' and datasets[dataset_idx] != 'mini_imagenet' and datasets[dataset_idx] != 'mini_imagenet_pkl':
             done = True
             print("using new dataset")
 
