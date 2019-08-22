5c19d6eb335cc1768d1cf347b45c6589322e9bbc
Jaewon Chung <jaywonchung@snu.ac.kr>
2019-08-22 10:35:45 +0000

    Put if __name__=="__main__" statement

--- script_generation_tools/generate_scripts.py
+++ script_generation_tools/generate_scripts.py
@@ -1,13 +1,6 @@
 import os
 from copy import copy
 
-local_script_dir = "../experiment_scripts"
-experiment_json_dir = '../experiment_config/'
-maml_experiment_script = 'train_maml_system.py'
-prefix = 'few_shot'
-
-if not os.path.exists(local_script_dir):
-    os.makedirs(local_script_dir)
 
 def load_template(filepath):
     with open(filepath, mode='r') as filereader:
@@ -29,19 +22,30 @@ def write_text_to_file(text, filepath):
     with open(filepath, mode='w') as filewrite:
         filewrite.write(text)
 
-local_script_template = load_template('local_run_template_script.sh')
 
-for subdir, dir, files in os.walk(experiment_json_dir):
-    for file in files:
-        if file.endswith('.json'):
-            config = file
+if __name__ == "__main__":
 
-            experiment_script = maml_experiment_script
+    local_script_dir = "../experiment_scripts"
+    experiment_json_dir = '../experiment_config/'
+    maml_experiment_script = 'train_maml_system.py'
+    prefix = 'few_shot'
 
-            local_script_text = fill_template(template_list=local_script_template,
-                                                execution_script=experiment_script,
-                                                experiment_config=file)
+    if not os.path.exists(local_script_dir):
+        os.makedirs(local_script_dir)
 
-            local_script_name = '../{}/{}_{}.sh'.format('experiment_scripts', file.replace(".json", ''), prefix)
-            local_script_name = os.path.abspath(local_script_name)
-            write_text_to_file(text=local_script_text, filepath=local_script_name)
+    local_script_template = load_template('local_run_template_script.sh')
+
+    for subdir, dir, files in os.walk(experiment_json_dir):
+        for file in files:
+            if file.endswith('.json'):
+                config = file
+
+                experiment_script = maml_experiment_script
+
+                local_script_text = fill_template(template_list=local_script_template,
+                                                    execution_script=experiment_script,
+                                                    experiment_config=file)
+
+                local_script_name = '../{}/{}_{}.sh'.format('experiment_scripts', file.replace(".json", ''), prefix)
+                local_script_name = os.path.abspath(local_script_name)
+                write_text_to_file(text=local_script_text, filepath=local_script_name)
