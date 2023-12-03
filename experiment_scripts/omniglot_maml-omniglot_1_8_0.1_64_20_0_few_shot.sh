05576aee1c6ce6a11518e242dc5457d7588579a5
Antreas Antoniou <iam@antreas.io>
2023-12-03 01:53:46 +0000

    remove omniglot images

--- experiment_scripts/omniglot_maml-omniglot_1_8_0.1_64_20_0_few_shot.sh
+++ experiment_scripts/omniglot_maml-omniglot_1_8_0.1_64_20_0_few_shot.sh
@@ -8,4 +8,4 @@ cd ..
 export DATASET_DIR="datasets/"
 export CUDA_VISIBLE_DEVICES=$GPU_ID
 # Activate the relevant virtual environment:
-python train_maml_system.py --name_of_args_json_file experiment_config/omniglot_maml-omniglot_1_8_0.1_64_20_0.json --gpu_to_use $GPU_ID
+python src/run.py --name_of_args_json_file experiment_config/omniglot_maml-omniglot_1_8_0.1_64_20_0.json --gpu_to_use $GPU_ID
